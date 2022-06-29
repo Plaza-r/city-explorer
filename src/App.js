40a8d29cf4dd284967bb573981db85db84f2ce2d
RIKI PLAZA <rikiplaza1@gmail.com>
2022-06-29 18:25:35 -0400

    finished lab07

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 import Display from './Display';
+import Weather from './Weather';
+
 
 class App extends React.Component {
   constructor(props) {
@@ -9,7 +11,8 @@ class App extends React.Component {
       city: '',
       cityData: {},
       error: false,
-      erroMessage: ''
+      erroMessage: '',
+      weatherData: []
     }
   }
 
@@ -17,7 +20,22 @@ class App extends React.Component {
     e.preventDefault();
     let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`;
     let cityInfo = await axios.get(url).catch(this.catch);
-    console.log(cityInfo);
+    
+    let cityForecast = await axios.get(`${process.env.REACT_APP_SERVER}/weatherData?searchQuery=${this.state.city}`).catch(err => {
+      console.log(err);
+    });//tailor to movie
+    console.log(cityForecast)
+    this.setState({
+      weatherData:cityForecast.data
+    }) 
+    
+    
+    // let forecast = [];
+    // if (!!cityForecast) {
+    //   forecast = cityForecast.data;
+    // }
+    
+    //console.log(forecast, 'forecast');
     if (!cityInfo) return
     this.setState({
       cityData: cityInfo.data[0],
@@ -29,6 +47,7 @@ class App extends React.Component {
 
    catch = (error) => {
     console.log(error, 'here is an error')
+    
     this.setState({
       error: true,
       errorMessage: `ERROR ${error.response.status}: Could not find ${this.state.city}`,
@@ -49,6 +68,8 @@ render(){
 console.log(this.state);
  
   return (   
+    <>
+
             
         <Display
           handleCityInput={this.handleCityInput}
@@ -56,7 +77,16 @@ console.log(this.state);
           error={this.state.error}
           errorMessage={this.state.errorMessage}
           cityData={this.state.cityData}
-        />    
+          weatherData={this.state.weatherData}
+        />  
+
+        {this.state.weatherData.length && 
+          <Weather
+            weatherData={this.state.weatherData}
+            city={this.state.city}
+          />
+        }  
+        </>
   
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Forecast from './Forecast';
+
+
+class Weather extends React.Component {
+    render() {
+
+        console.log(this.props.weatherData);
+
+        let weather = this.props.weatherData.map((day, idx) => (
+            <Forecast
+                description={day.description}
+                key={idx}
+                city={this.props.city}
+                date={day.date}
+            />
+        ))
+        return (
+            <div className="border-top py-4">
+                <h3>Weather</h3>
+
+                {!weather.length ? (
+                    <p>No Weather Available</p>
+                ) : (
+                    <ul className='list-unstyled '>
+                        {[weather]}
+                    </ul>
+                )}
+            </div>
+        );
+    }
+}
+
+export default Weather;
