ffade1dff50f07899e18babdf4376213932ae569
RIKI PLAZA <rikiplaza1@gmail.com>
2022-07-01 11:37:33 -0400

    checking if works

--- src/Weather.js
+++ src/Weather.js
@@ -1,34 +1,26 @@
-import React from 'react';
-import Forecast from './Forecast';
+const axios = require('axios');
 
+async function getWeather (cityName) {
+    try {
+        
+        let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${cityName}&key=${process.env.WEATHER_API_KEY}&days=3&lat=23&lon=155`;
+        let cityWeather = await axios.get(url);
+        let selectedCity = cityWeather.data.data.map(dailyWeather => {
+            return new Forecast(dailyWeather);
+        });
+    console.log(selectedCity);
+        return selectedCity;
+    } catch (error) {
+        console.log(error.message);
+    }
 
-class Weather extends React.Component {
-    render() {
+}
 
-        console.log(this.props.weatherData);
-
-        let weather = this.props.weatherData.map((day, idx) => (
-            <Forecast
-                description={day.description}
-                key={idx}
-                city={this.props.city}
-                date={day.date}
-            />
-        ))
-        return (
-            <div className="border-top py-4">
-                <h3>Weather</h3>
-
-                {!weather.length ? (
-                    <p>No Weather Available</p>
-                ) : (
-                    <ul className='list-unstyled '>
-                        {[weather]}
-                    </ul>
-                )}
-            </div>
-        );
+class Forecast {
+    constructor(cityWeather) {
+        this.date = cityWeather.datetime;
+        this.description = cityWeather.weather.description;
     }
 }
 
-export default Weather;
+module.exports = getWeather;
