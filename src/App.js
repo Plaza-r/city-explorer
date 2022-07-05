8ebe046e27208b89b3ec8d4cd3bbc99f76f4a22f
RIKI PLAZA <rikiplaza1@gmail.com>
2022-07-04 23:42:10 -0400

    finished lab 8 and refactored for 9

--- src/App.js
+++ src/App.js
@@ -2,75 +2,81 @@ import React from 'react';
 import axios from 'axios';
 import Display from './Display';
 import Weather from './Weather';
-
+import Movies from './Movies';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+ constructor(props) {
+  super(props) 
     this.state = {
       city: '',
-      cityData: {},
+      cityData: 0,
       error: false,
-      erroMessage: '',
-      weatherData: []
+      errorMessage: '',
+      weatherData: [],
+      movieArray: []
     }
-  }
+  }  
 
   handleCitySubmit = async(e) => {
     e.preventDefault();
-    let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`;
-    let cityInfo = await axios.get(url).catch(this.catch);
-    
-    let cityForecast = await axios.get(`${process.env.REACT_APP_SERVER}/weatherData?searchQuery=${this.state.city}`).catch(err => {
-      console.log(err);
-    });//tailor to movie
-    console.log(cityForecast)
-    this.setState({
-      weatherData:cityForecast.data
-    }) 
-    
-    
-    // let forecast = [];
-    // if (!!cityForecast) {
-    //   forecast = cityForecast.data;
-    // }
-    
-    //console.log(forecast, 'forecast');
-    if (!cityInfo) return
-    this.setState({
-      cityData: cityInfo.data[0],
-      error: false,
-      errorMessage: ''
-    })
+    try {
+      let data =  await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`);
 
+      let cityDataMap = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${data.data[0].lat},${data.data[0].lon}&zoom=14`
+
+      console.log(data, 'data from locationiq api')
+
+      const { lon, lat } = data.data[0];
+
+      console.log(lat, lon) 
+
+      const cityForecast = await axios.get(`${process.env.REACT_APP_SERVER}/weather?lat=${lat}&lon=${lon}`);
+
+      console.log(cityForecast, 'new weather request')
+
+
+
+
+      let forecast = cityForecast.data;
+      console.log(cityForecast);
+      let url = `${process.env.REACT_APP_SERVER}/movies?movieQueryCity=${this.state.city}`
+      let cityMovie = await axios.get(url);
+      let movieData = cityMovie.data
+
+
+      this.setState ({
+        cityData: data.data[0],
+        flag:true,
+        img: cityDataMap,
+        weatherData: forecast,
+        movieArray: movieData
+      })
+
+
+    } catch (error) {
+
+      this.setState({
+        error:true,
+        errorMessage:`An Error Occurred: ${error.response.status}`
+
+      })
+
+    }
+
+    
   }
-
-   catch = (error) => {
-    console.log(error, 'here is an error')
-    
-    this.setState({
-      error: true,
-      errorMessage: `ERROR ${error.response.status}: Could not find ${this.state.city}`,
-      cityData: {}
-    })
-
-  }
-
   handleCityInput = (e) => {
     this.setState({
       city: e.target.value
     })
-
   }
 
 render(){
 
-console.log(this.state);
- 
-  return (   
-    <>
 
-            
+  return (   
+        
+      <>
         <Display
           handleCityInput={this.handleCityInput}
           handleCitySubmit={this.handleCitySubmit}
@@ -78,18 +84,49 @@ console.log(this.state);
           errorMessage={this.state.errorMessage}
           cityData={this.state.cityData}
           weatherData={this.state.weatherData}
-        />  
+        /> 
+        
+        
 
-        {this.state.weatherData.length && 
-          <Weather
-            weatherData={this.state.weatherData}
-            city={this.state.city}
-          />
-        }  
-        </>
+          {this.state.movieArray.length &&
+            <Movies
+              movie={this.state.movieArray}
+            />
+          }
+
+        
+      </>
   
   );
 }
 };
 export default App;
-//seeing if works
+//seeing it works
+
+
+//***** have to refactor */
+ // handleCitySubmit = async(e) => {
+  //   e.preventDefault();
+  //   let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`;
+
+  //   let cityInfo = await axios.get(url).catch(this.catch);
+
+  //   let cityForecast = await axios.get(`${process.env.REACT_APP_SERVER}/weather?searchQueryCity=${this.state.city}`).catch(err => {
+  //     console.log(err);
+  //   });
+
+  //   let forecast = [];
+  //   if (!!cityForecast) {
+  //     forecast = cityForecast.data;
+  //   }refectored
+
+  //   console.log(forecast, 'forecast');
+  //   if (!cityInfo) return
+  //   this.setState({
+  //     cityData: cityInfo.data[0],
+  //     error: false,
+  //     errorMessage: '',
+  //     weatherData: forecast
+  //   }) refactored
+
+ 
--- src/Display.js
+++ src/Display.js
@@ -1,33 +1,33 @@
-import React from "react";
-import Form from './Form';
-import Error from "./Error";
-
+import React from 'react';
+import Forms from './Forms';
+import Error from './Error';
+import Weather from './Weather';
 
 class Display extends React.Component {
-  render() {
-    console.log(this.props, 'props');
+  render () {
+console.log(this.props, 'props');
     return (
-      <div className='d-flex'>
+      <div className='d-flex h-100' style={{textAlign:'left',overflow:'scroll'}}>
 
-        <div className='d-flex justify-content-center align-items-center' style={{ background: '#A0C8F1', width: '67%', height: '100vh' }}>
+        <div className='d-flex justify-content-center align-items-center' style={{background: '#A0C8F0', width: '67%', height:'auto'}}>
           {Object.keys(this.props.cityData).length > 0 && (
             <img
               alt={this.props.cityData.display_name}
-              src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.props.cityData.lat},${this.props.cityData.lon}&zoom=10`}
+              src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.props.cityData.lat},${this.props.cityData.lon}&size=600x600&zoom=10`}
             />
           )}
         </div>
 
 
-        <div className='px-5 py-3 text-light' style={{ background: '#3E688A', width: '33%', height: '100vh' }}>
+        <div className='px-5 py-3 text-light'style={{background: '#3E688A', width: '33%', height:'fit-content'}}>
           <h1>City Explorer</h1>
-          <Form
+          <Forms
             handleCityInput={this.props.handleCityInput}
             handleCitySubmit={this.props.handleCitySubmit}
           />
 
           {this.props.error && (
-            <Error
+            <Error 
               errorMessage={this.props.errorMessage}
             />
 
@@ -35,39 +35,35 @@ class Display extends React.Component {
 
           {Object.keys(this.props.cityData).length > 0 && (
             <>
-            <ul className='list-unstyled '>
-              <li className='py-2'>
-                <p className='p-0 m-0 '>Latitude</p> <p className='p-0 m-0 h3'>{this.props.cityData.lat}</p>
-              </li>
+              <ul className='list-unstyled'>
+                <li className='py-2'>
+                  <p className='p-0 m-0 '>Latitude</p> <p className='p-0 m-0 h3'>{this.props.cityData.lat}</p>
+                </li>
 
-              <li className='py-2'>
-                <p className='p-0 m-0 '>Longtitude</p> <p className='p-0 m-0 h3'>{this.props.cityData.lon}</p>
-              </li>
+                <li className='py-2'>
+                  <p className='p-0 m-0 '>Longtitude</p> <p className='p-0 m-0 h3'>{this.props.cityData.lon}</p>
+                </li>
 
-              <li className='py-2'>
-                <p className='p-0 m-0 '>Location</p> <p className='p-0 m-0 h3'>{this.props.cityData.display_name}</p>
-              </li>
+                <li className='py-2'>
+                  <p className='p-0 m-0 '>Location</p> <p className='p-0 m-0 h3'>{this.props.cityData.display_name}</p>
+                </li>
 
 
-            </ul>
-            {/* <Weather
-            weatherData={this.props.weatherData}
-            city={this.props.cityData.display_name}
-
-            /> */}
+              </ul>
+              <Weather
+                weatherData={this.props.weatherData}
+                city={this.props.cityData.display_name} 
+              />
             </>
-
           )}
         </div>
 
-
-
-
-
+  
+        
+        
+          
       </div>
     )
   }
 }
-
 export default Display;
-//checcking
